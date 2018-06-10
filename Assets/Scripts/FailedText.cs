ab8b55c8b4e4e0292664545eefc514e9bb071636
oumiya <oumiya.s676@gmail.com>
2018-06-10 19:47:07 +0900

    敵に見つかった時の演出を強化

---
+++ Assets/Scripts/FailedText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FailedText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Text>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.Scene == Player.Scenes.FadeOut)
+        {
+            GetComponent<Text>().enabled = true;
+        }
+        if (Player.Scene == Player.Scenes.FadeIn)
+        {
+            GetComponent<Text>().enabled = false;
+        }
+    }
+}
