14b3b4d2cb1ff08230f31f264817002fc84778aa
oumiya <oumiya.s676@gmail.com>
2018-06-10 16:28:34 +0900

    主人公の動きにカメラを追従させた

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    GameObject player;
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // カメラと主人公の位置の差分を offset に保存しておく
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition.x = player.transform.position.x + offset.x;
+        newPosition.y = player.transform.position.y + offset.y;
+        newPosition.z = player.transform.position.z + offset.z;
+
+        transform.position = Vector3.Lerp(transform.position, newPosition, 5.0f * Time.deltaTime);
+    }
+}
