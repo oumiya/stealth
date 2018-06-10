31accd3111c7f9d39d343f71afc64b86f342d754
oumiya <oumiya.s676@gmail.com>
2018-06-10 15:54:30 +0900

    箱を動かせるようにした
    ・箱と地面を設置
    ・地面にマテリアルを設置して適当に地面を設置

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    float speed = 3.0f;
+    float gravity = 9.81f;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    CharacterController characterController;
+
 	// Use this for initialization
 	void Start () {
-		
+        characterController = GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
 	}
 }
